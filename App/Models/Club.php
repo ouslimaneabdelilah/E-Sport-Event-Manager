ee6d599c582155ed9f9644d4513fd29bcbfafeb6
ouslimaneabdelilah <abdelilah.ouslimane@gmail.com>
2025-12-24 22:54:21 +0100

    feat: Ajouter getters et setters pour model club avec les attributes

--- App/Models/Club.php
+++ App/Models/Club.php
@@ -1,13 +1,33 @@
 <?php
 namespace App\Models;
+
+use App\Engine\Column;
+use App\Engine\Table;
+#[Table(name:"clubs")]
 class Club{
-    public int $id;
-    public string $nom;
-    public string $ville;
-    public int $created_at;
+    #[Column(name:'id',type:'INT',primaryKey:true)]
+    private int $id;
+    #[Column(name:'nom',type:'VARCHAR',length:50)]
+    private string $nom;
+    #[Column(name:'ville',type:'VARCHAR')]
+    private string $ville;
+    #[Column(name:'string',type:'TIMESTAMP')]
+    private string $created_at;
     public function __construct(string $nom,string $ville)
     {
         $this->nom = $nom;
         $this->ville = $ville;
+    }
+    public function setName($nom){
+        $this->nom = $nom;
+    }
+    public function setVille($ville){
+        $this->ville = $ville;
+    }
+    public function getVille(){
+        return $this->ville;
+    }
+    public function getName(){
+        return $this->ville;
     }
 }
