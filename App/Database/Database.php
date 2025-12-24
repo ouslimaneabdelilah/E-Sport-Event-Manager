ea4ab5967ce916e97051a75279809689cc32ee15
ouslimaneabdelilah <abdelilah.ouslimane@gmail.com>
2025-12-24 10:21:11 +0100

    feat: ajouter la classe Database pour la gestion de la connexion à la base de données

---
+++ App/Database/Database.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Database;
+abstract class Database{
+   private static Database $instance = null;
+   private \PDO $connexion;
+   private string $username = "root";
+   private string $host = "localhost";
+   private string $db_name = "esport_db";
+   private string $password = "";
+   private function __construct()
+   {
+        $this->connexion = new \PDO("mysql:host={$this->host};dbname={$this->db_name}",
+            $this->username,
+            $this->password);
+   }
+   public static function getInstance(){
+    if(self::$instance === null) self::$instance = new Database();
+    return self::$instance;
+   }
+
+   public function gatConnection(){
+    return $this->connexion;
+   }
+}
+?>
